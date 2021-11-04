fa5af97ba6697fd216dde66522d25409e1c74522
Casey Sloan <clsloan2075@eagle.fgcu.edu>
2021-11-04 16:24:48 -0400

    added back in my comments on ToDo.razor & TodoItem.cs

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,5 +1,8 @@
 public class TodoItem
-{
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+{ //creates a class for itemizing the Todo list per item
+
+    public string Title { get; set; } //Title is a property | get & set are the methods
+                                      //a property is kind of like a field, or kind of like a method
+                                      // This allows for the inputted text of said item Todo
+    public bool IsDone { get; set; } // This allows for the check off once complete
 }
