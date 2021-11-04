22dc8f2910f5d2fc60f6d0afc46fcbd04dc39ab6
Casey Sloan <clsloan2075@eagle.fgcu.edu>
2021-11-04 16:20:52 -0400

    Modified icons

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,8 +1,5 @@
 public class TodoItem
-{ //creates a class for itemizing the Todo list per item
-
-    public string Title { get; set; } //Title is a property | get & set are the methods
-                                      //a property is kind of like a field, or kind of like a method
-                                      // This allows for the inputted text of said item Todo
-    public bool IsDone { get; set; } // This allows for the check off once complete
+{
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
 }
